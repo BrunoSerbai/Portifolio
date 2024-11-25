5c58de2d00035b38bd81af956ee4c6b106e3604d
Bruno Serbai <brunoserbai@outlook.com>
2024-11-24 23:58:41 -0300

    Add whatsapp message when user opens the website

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+const phoneNumber = "554391107327";
+const message = encodeURIComponent("Hello! 😃%0ASomeone just visited your website! 🥳");
+const apiKey = "2529632";
+const url = `https://api.callmebot.com/whatsapp.php?phone=${phoneNumber}&text=${message}&apikey=${apiKey}`;
+
+fetch(url)
+    .then(response => {
+        if (response.ok) {
+            console.log("Message sent successfully!");
+        } else {
+            console.error("Error sending message:", response.statusText);
+        }
+    })
+    .catch(error => console.error("Fetch error:", error));
+
+    
