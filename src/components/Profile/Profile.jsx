4331ca032b44c7019a8ccfe42d03e0136902445a
Bruno Serbai <brunoserbai@outlook.com>
2025-01-03 00:04:00 -0300

    fix icons

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import ProfileImg from "../../assets/Images/b2.jpg";
+import profileImg from "../../assets/Images/b2.jpg";
+import contactImg from "../../assets/Icons/tel1.svg"
+import cvImg from "../../assets/Icons/document.svg"
 import "./Profile.css";
 
 function Profile() {
   return (
     <>
-      <img id="profile" src={ProfileImg} />
+      <img id="profile" src={profileImg} />
       <h1 className="name">Bruno Serbai</h1>
-      <div className="header-links">
+      <div className="devicon">
         <a href="#contato">
-          <button>☎ Contato</button>
+          <button className="links"><img src={contactImg} /> Contato</button>
         </a>
         <a target="blank" href="src/assets/documents/Bruno_Serbai.doc">
-          <button className="outline fix"> 🖹 Currículo</button>
+          <button className="outline links"> <img src={cvImg}/>  Currículo</button>
         </a>
       </div>
     </>
